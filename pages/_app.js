0a13e8c7af3818eb8342c9712d6a567a7a31f86a
ElkSeth <seth@elklabs.org>
2022-10-05 02:00:06 -0400

    ssr

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,9 @@ import '../styles/globals.css';
 import '../styles/App.scss';
 import '../styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { SSRProvider } from "react-bootstrap";
 
 function MyApp({ Component, pageProps }) {
-  return <SSRProvider><Component {...pageProps} /></SSRProvider>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
